6aa53209a46560cadc1d4907462ca320b5803bc2
Ariel_Santangelo <arielsantangelo610@gmail.com>
2023-05-12 22:55:21 -0300

    code refactorized

--- src/main/java/com/ariel/ProductsApi/Abstractions/BaseEntity.java
+++ src/main/java/com/ariel/ProductsApi/Abstractions/BaseEntity.java
@@ -1,19 +1,13 @@
 package com.ariel.ProductsApi.Abstractions;
 
-import java.io.Serializable;
 import java.sql.Date;
 
-import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-
-import jakarta.persistence.AttributeOverride;
 import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -28,11 +22,9 @@ public abstract class BaseEntity {
     @GeneratedValue(strategy=GenerationType.AUTO)
     protected long id;
 
-    @CreatedDate
     @Column(name="created_at")
     protected Date createdAt;
 
-    @UpdateTimestamp
     @Column(name="updated_at")
     protected Date updatedAt;
 
@@ -47,6 +39,8 @@ public abstract class BaseEntity {
     protected boolean status;
 
     public BaseEntity(String createdBy, String updatedBy, boolean status) {
+        this.createdAt = new Date(System.currentTimeMillis());
+        this.updatedAt = new Date(System.currentTimeMillis());
         this.createdBy = createdBy;
         this.updatedBy = updatedBy;
         this.status = status;
--- src/main/java/com/ariel/ProductsApi/Abstractions/ValidationController.java
+++ src/main/java/com/ariel/ProductsApi/Abstractions/ValidationController.java
@@ -1,10 +1,7 @@
 package com.ariel.ProductsApi.Abstractions;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
